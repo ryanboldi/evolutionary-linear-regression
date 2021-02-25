784f9a818a975b8c84ee8c2fcbd49cb5dd945d39
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-25 21:56:41 +0400

    added stats library

--- main.clj
+++ main.clj
@@ -1,4 +1,6 @@
 (ns main)
+(require 'incanter.stats)
+(alias 'inc 'incanter.stats)
 
 (def vertices
   (list {:x 1, :y 1}
@@ -7,6 +9,8 @@
         {:x 5, :y 6}))
 
 (def parameters 2) ; y = ax + b
+(def mutation-rate 0.2)
+(def mutation-size 0.2) ; sd of the normal sampling
 
 (defn random-solution
   "returns a random solution"
@@ -23,13 +27,19 @@
   [solution vert]
   (reduce * (repeat 2 (- (:y vert) (get-y solution (:x vert))))))
 
-
 (defn assess-solution
   "returns the fitness of a solution"
   [solution]
-  ())
+  (reduce + (map compare-vert (repeat solution (count vertices)) vertices)))
 
-(map #(get-y) vertices)
+(defn mutate-solution
+  [solution]
+  (zipmap [:a :b] [(inc/sample-normal 1 :mean (:a solution) :sd mutation-size) (inc/sample-normal 1 :mean (:b solution) :sd mutation-size)]))
+
+(defn cross-solutions
+  [s1 s2]
+  (zipmap [:a :b] [(:a s1) (:b s2)]))
+
 
 
 (defn -main
--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject evolutionary-linear-regression "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [incanter "1.9.3"]]
   :source-paths ["."]
   :main main)
